274732ecdbb98469cec579a398a0d5a0f68c3d9f
Rick Matta <rickmatta@gmail.com>
2015-08-20 23:41:25 -0400

    beginning

---
+++ FreeFlow/FreeFlow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FreeFlow
+//
+//  Created by rick m on 8/20/15.
+//  Copyright (c) 2015 FreeFlowAppDevelopers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FreeFlow/FreeFlow/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  FreeFlow
+//
+//  Created by rick m on 8/20/15.
+//  Copyright (c) 2015 FreeFlowAppDevelopers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
