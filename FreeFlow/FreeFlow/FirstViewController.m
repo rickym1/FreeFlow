a4c46f6d618823111b950a60e07770e76977ffa3
Rick Matta <rickmatta@gmail.com>
2015-08-27 12:58:11 -0400

    visualization hooked up but needs refactor and sound

--- FreeFlow/FreeFlow/FirstViewController.m
+++ FreeFlow/FreeFlow/FirstViewController.m
@@ -14,6 +14,9 @@
 @property (weak, nonatomic) IBOutlet UITextField *factorTextView;
 @property (weak, nonatomic) IBOutlet UILabel *secondsLabelView;
 
+@property (nonatomic, assign) CGFloat dropsPerMinute;
+@property (nonatomic, assign) BOOL normalBackground;
+
 @end
 
 @implementation FirstViewController
@@ -27,6 +30,8 @@
     UITapGestureRecognizer *gestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(hideKeyboard)];
     gestureRecognizer.cancelsTouchesInView = NO;
     [self.view addGestureRecognizer:gestureRecognizer];
+    
+    self.normalBackground = YES;
     
 }
 
@@ -49,6 +54,18 @@
         sender.text = nil;
     }
     self.factorTextView.text = enteredText;
+    
+}
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string  {
+    
+    
+    NSCharacterSet *cs = [[NSCharacterSet characterSetWithCharactersInString:@"0123456789."] invertedSet];
+    
+    NSString *filtered = [[string componentsSeparatedByCharactersInSet:cs] componentsJoinedByString:@""];
+    
+    return [string isEqualToString:filtered];
+    
 }
 
 -(void) hideKeyboard {
@@ -58,17 +75,48 @@
 -(BOOL) textFieldShouldReturn:(UITextField *)textField{
     
     [textField resignFirstResponder];
+    [self calculateRate];
+    [self playRate];
+    
+    
     return YES;
 }
 
 - (void)calculateRate {
     
-    if (self.rateTextView.text && self.factorTextView.text != nil) {
-        NSLog(@"It's Calculating Time!");
+    if (self.rateTextView.text.length > 0 && self.factorTextView.text.length > 0) {
+        
+        float rate = [self.rateTextView.text floatValue];
+        float factor = [self.factorTextView.text floatValue];
+        float dropsPerHour = rate * factor;
+        float dropsPerSecond = dropsPerHour / 3600;
+        self.dropsPerMinute = dropsPerHour / 60;
+        float secondsPerDrop = 1 / dropsPerSecond;
+        
+        NSString *secondsString = [NSString stringWithFormat:@"= 1 drop every %.00f seconds", secondsPerDrop];
+        self.secondsLabelView.text = secondsString;
         
     }
 }
 
+
+-(void)playRate {
+    
+    if (self.rateTextView.text.length > 0 && self.factorTextView.text.length > 0) {
+        
+        if (self.normalBackground) {
+            self.view.backgroundColor = [UIColor whiteColor];
+            self.normalBackground = NO;
+        } else {
+            self.view.backgroundColor = [UIColor colorWithRed:0x1B/255.0f green:0xB6/255.0f blue:0xFF/255.0f alpha:1];
+            self.normalBackground = YES;
+        }
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [NSTimer scheduledTimerWithTimeInterval:(60/self.dropsPerMinute) target:self selector:@selector(playRate) userInfo:nil repeats:NO];
+        });
+    }
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
